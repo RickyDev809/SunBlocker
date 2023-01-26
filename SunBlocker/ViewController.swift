bfda2a800d460443e6cc3b44eb7ddbcfbf478a26
Ricardo <rickydev809@gmail.com>
2023-01-26 16:34:24 -0600

    Fixed bug that showed "Reapply" anytime the user returned from the app even if the timer still had time left. Now it will only show reapply if the time runs to 0. Also added sound notification so that when the timer is up it will alert the user. Moved start button code next to their other buttons so it is more organized.

--- SunBlocker/ViewController.swift
+++ SunBlocker/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import UserNotifications
+import AVFoundation
 
 class ViewController: UIViewController, UNUserNotificationCenterDelegate {
     
@@ -36,11 +37,10 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         customBtn.layer.masksToBounds = true
         sweatSwimBtn.layer.cornerRadius = sweatSwimBtn.frame.width / 2
         sweatSwimBtn.layer.masksToBounds = true
-        
         sweatSwimBtn.titleLabel?.numberOfLines = 0
         sweatSwimBtn.titleLabel?.adjustsFontSizeToFitWidth = true
         sweatSwimBtn.titleLabel?.lineBreakMode = .byWordWrapping
-   
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -63,45 +63,27 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         
     }
     
+    //This function is used to calculate the timer when the user leaves the app and returns to it. If the user suspends the app or switches to another one this calculation is used to notify the user when it has been completed.
+    
     @objc private func applicationDidBecomeActive() {
-        if seconds < 1 {
-            timer?.invalidate()
-        } else {
-            if let lastTimerStartTime = UserDefaults.standard.value(forKey: "lastTimerStartTime") as? Date,
-               let totalTime = UserDefaults.standard.value(forKey: "totalTime") as? Double {
-                let timeSinceStartTime = Date() - lastTimerStartTime
-                if timeSinceStartTime < totalTime {
-                    seconds = totalTime - timeSinceStartTime
-                    runTimer()
-                }
-            } else {
-                timeLabel.text = timeString(time: TimeInterval(seconds))
-            }
-            notifyTimerCompleted()
-        }
-    }
-    
-    //This begins the timer after user has selected on of the options of time.
-    
-    @IBAction func startBtn(_ sender: UIButton) {
         
-        if seconds < 1 {
-            timer?.invalidate()
-        } else {
-            if isTimerRunning == false {
+        if let lastTimerStartTime = UserDefaults.standard.value(forKey: "lastTimerStartTime") as? Date,
+           let totalTime = UserDefaults.standard.value(forKey: "totalTime") as? Double {
+            let timeSinceStartTime = Date() - lastTimerStartTime
+            if timeSinceStartTime < totalTime {
+                seconds = totalTime - timeSinceStartTime
                 runTimer()
+            } else {
+                seconds = 0
+                timeLabel.text = timeString(time: TimeInterval(seconds))
+                notifyTimerCompleted()
             }
-            
-            UNUserNotificationCenter.current().delegate = self
-            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in
-                guard success else {
-                    return
-                }
-            }
+        } else {
+            timeLabel.text = timeString(time: TimeInterval(seconds))
         }
-        self.plusBtn.isHidden = true
-        self.minusBtn.isHidden = true
     }
+    
+    //Runs the timer, performs calcaluations from when the user first presses start until timer is completed.
     
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
@@ -113,11 +95,14 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         addNotification()
     }
     
+    //Notification that shows when timer is up.
+    
     func addNotification() {
         
         let content = UNMutableNotificationContent()
         content.title = "Timer Finished"
         content.body = "Time to Reapply!"
+        content.sound = .default
         var trigger: UNTimeIntervalNotificationTrigger
         trigger = UNTimeIntervalNotificationTrigger(timeInterval: seconds, repeats: false)
         let notification = UNNotificationRequest(identifier: "timer", content: content, trigger: trigger)
@@ -127,23 +112,48 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         
     }
     
+    //Once the time is completed this updates the time label text to remind the user to apply sunscreen.
+    
     func notifyTimerCompleted() {
         timeLabel.text = "Apply Suncreen!"
+        
     }
+    
+    // This funciton updates the timer. Either it continues the timer or invalidates it when it reaches 0.
     
     @objc func updateTimer(){
         if seconds < 1 {
             timer?.invalidate()
             notifyTimerCompleted()
             
-            // Send alert to indicate "Reapply!"
         } else {
             seconds -= 1
             timeLabel.text = timeString(time: TimeInterval(seconds))
         }
     }
     
-    //This resets the timer to 0
+    //This is the start button which begins the timer after the user has selected the time they want to be reminded. Includes code that invalidates time if there is one already running.
+    
+    @IBAction func startBtn(_ sender: UIButton) {
+        
+        if seconds < 1 {
+            timer?.invalidate()
+        } else {
+            if isTimerRunning == false {
+                runTimer()
+            }
+            UNUserNotificationCenter.current().delegate = self
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in
+                guard success else {
+                    return
+                }
+            }
+        }
+        self.plusBtn.isHidden = true
+        self.minusBtn.isHidden = true
+    }
+    
+    //This resets the timer to 0 when user presses reset button.
     
     @IBAction func resetBtn(_ sender: UIButton) {
         timer?.invalidate()
@@ -154,13 +164,11 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         self.minusBtn.isHidden = true
     }
     
-    
-    
     // Auto timer sets the timer to 2 hours which is the recommened time to reapply sunscreen.
     
     @IBAction func autoBtn(_ sender: UIButton) {
         timer?.invalidate()
-        seconds = 3600
+        seconds = 7200
         timeLabel.text = timeString(time: TimeInterval(seconds))
         isTimerRunning = false
         self.plusBtn.isHidden = true
@@ -199,7 +207,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         timeLabel.text = timeString(time: TimeInterval(seconds))
     }
     
-    // TimeString Function
+    // TimeString Function that calculates the hours,minutes and seconds for the timer.
     
     func timeString(time:TimeInterval) -> String {
         let hours = Int(time) / 3600
@@ -214,3 +222,4 @@ extension Date {
         return lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate
     }
 }
+
